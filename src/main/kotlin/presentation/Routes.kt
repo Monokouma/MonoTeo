9e8d6f159b7cf43f5bd7513e486233f752bd2119
monokouma <haaxiz@gmail.com>
2025-12-17 00:08:50 +0100

    Add contentent negociation

--- src/main/kotlin/presentation/Routes.kt
+++ src/main/kotlin/presentation/Routes.kt
@@ -1,7 +1,15 @@
 package com.despaircorp.presentation
 
+import io.ktor.http.ContentType
+import io.ktor.server.application.call
+import io.ktor.server.response.respondBytes
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 
 fun Route.getRoutes() {
 
+    get("/health") {
+        call.respondText("resource.readBytes()", ContentType.Text.Plain)
+    }
 }
